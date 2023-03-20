f578ad3c26a7a6a9d2ad78f4cf56d35378637906
dangvuhiep7500 <dangvuhiep1291@gmail.com>
2023-03-20 22:53:07 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,53 @@
+'use client';
+
+import React from "react";
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import { Head } from 'next/document'
+import { Dropdown } from "flowbite-react";
+import { NextPage } from "next";
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">
-    Hello world!
-  </h1>
+    <main >
+      <h1 >
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+
+      <p >
+        Get started by editing{' '}
+        <code >pages/index.js</code>
+      </p>
+
+      <div>
+        <a href="https://nextjs.org/docs">
+          <h2>Documentation &rarr;</h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a href="https://nextjs.org/learn">
+          <h2>Learn &rarr;</h2>
+          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        </a>
+
+        <a
+          href="https://github.com/vercel/next.js/tree/master/examples"
+          
+        >
+          <h2>Examples &rarr;</h2>
+          <p>Discover and deploy boilerplate example Next.js projects.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        >
+          <h2>Deploy &rarr;</h2>
+          <p>
+            Instantly deploy your Next.js site to a public URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
   )
 }
