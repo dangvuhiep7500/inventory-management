dd5a3a37caecf8488a3dbab58c98010c8cc47982
dangvuhiep7500 <dangvuhiep1291@gmail.com>
2023-05-16 17:35:46 +0700

    fix deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
     <a href="/auth/signin" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
-       <header>
+       {/* <header>
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="https://flowbite.com" className="flex items-center">
@@ -288,7 +288,7 @@ export default function Home() {
                 </div>
             </div>
         </div>
-    </footer>
+    </footer> */}
     </>
   );
 }
