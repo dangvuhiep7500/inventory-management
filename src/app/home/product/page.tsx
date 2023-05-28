d61d2b10cf43c1e08d0c56a14bc38d2dfc2e69a2
dangvuhiep7500 <dangvuhiep1291@gmail.com>
2023-05-28 21:46:49 +0700

    fix

--- src/app/home/product/page.tsx
+++ src/app/home/product/page.tsx
@@ -114,18 +114,18 @@ export default function PageProduct() {
               >
                 <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
               </svg>
-              <Typography className="">Admin</Typography>
+              <span className="">Admin</span>
             </div>
           </a>
           <a href="#" className="text-black dark:text-white">
-            <Typography className="hover:text-light-blue-500">
+            <span className="hover:text-light-blue-500">
               Sản phẩm
-            </Typography>
+            </span>
           </a>
           <a href="#" className="text-black dark:text-white">
-            <Typography className="hover:text-light-blue-500">
+            <span className="hover:text-light-blue-500">
               Tạo sản phẩm
-            </Typography>
+            </span>
           </a>
         </Breadcrumbs>
         <div>
